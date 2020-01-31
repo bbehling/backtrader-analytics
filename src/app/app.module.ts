a9ce7d04283bdf27bd2d3ef173ecc7a715e0f4f4
brian behling <brian.behling@gmail.com>
2020-01-31 16:38:16 -0700

    Added date controls, single or date range query, and paginated table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { QueryInputComponent } from "./query-input/query-input.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {
+  MatInputModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatNativeDateModule,
+  MatSortModule
+} from "@angular/material";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, QueryInputComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
+
+@Injectable({
+  providedIn: "root"
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getGolenCrossData(data) {
+    return this.http.post(
+      `${environment.apiUrl}/goldencross`,
+      JSON.stringify(data),
+      {
+        headers: { "Content-Type": "application/json" }
+      }
+    );
+  }
+}
